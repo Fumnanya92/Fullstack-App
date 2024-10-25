ef93e4249339800ad35e0bb7f70de7363a62dd19
Nonsocha <nonsowilliams021@gmail.com>
2024-10-25 01:05:32 +0100

    UPdate

---
+++ backend/test/example.test.js
@@ -0,0 +1,12 @@
+// tests/example.test.js
+
+// Example function to test
+function sum(a, b) {
+    return a + b;
+  }
+  
+  // Jest test for the sum function
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  
